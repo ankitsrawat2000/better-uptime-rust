09d3028f0514bd84787f1a2a2403243ec2b0bde4
ankitsrawat2000 <ankitsrawat2000@gmail.com>
2025-07-22 23:08:25 +0530

    sign in and sign up endpoint

--- store/src/models/user.rs
+++ store/src/models/user.rs
@@ -29,8 +29,18 @@ impl Store{
         Ok(id.to_string())
     }
     
-    pub fn sign_in(&self, username: String, password: String){
+    pub fn sign_in(&mut self, input_username: String, input_password: String) -> Result<bool,diesel::result::Error>{
+        use crate::schema::user::dsl::*;
+        let user_result = user
+            .filter(username.eq(input_username))
+            .select(User::as_select())
+            .first(&mut self.conn)?;
 
+        if user_result.password != input_password {
+            return Ok(false);
+        }
+
+        Ok(true)
     }
     
 }
--- store/src/models/website.rs
+++ store/src/models/website.rs
@@ -1,4 +1,17 @@
 use crate::store::Store;
+use diesel::prelude::*;
+use uuid::Uuid;
+
+#[derive(Queryable, Insertable, Selectable)]
+#[diesel(table_name = crate::schema::website)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+
+struct User{
+    id: String,
+    url: String,
+    user_id : String,
+    time_added: chrono::NaiveDateTime,
+}
 
 impl Store{
     pub fn create_website(&self){
